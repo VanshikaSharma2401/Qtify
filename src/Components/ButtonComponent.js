19a58ea3d2da169d6f1805221ff1d4d600d09a1d
VanshikaSharma2401 <bulbulsharma2401@gmail.com>
2024-02-25 19:51:33 +0530

    test case fix

--- src/Components/ButtonComponent.js
+++ src/Components/ButtonComponent.js
@@ -1,11 +1,9 @@
 import React from "react";
 const Button = ({text}) => {
   return (
-    <div className="btn">
       <button className="Feedback-btn" type="text">
         {text}
       </button>
-    </div>
   );
 };
 export default Button;
