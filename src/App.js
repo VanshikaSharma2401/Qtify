cebcf3a8266e82196b016b8d51fded435bde2808
VanshikaSharma2401 <bulbulsharma2401@gmail.com>
2024-02-26 23:07:00 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import Hero from "./components/Hero/hero";
 import Navbar from "./components/Navbar/navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Hero />
     </div>
   );
 }
